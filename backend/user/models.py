cf99aa93751391a008c65dfef56969a395bcbbf8
mieraf <elaman99619@gmail.com>
2025-12-25 16:00:47 +0300

    add customUser and following logic

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CustomUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
+    bio = models.CharField(max_length=200)
+    follow = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        through='Follow',
+        related_name='followers'
+    )
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following_set')
+    following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='follower_set')

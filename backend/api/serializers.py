562fce830af29904f366fce03627f61451835699
mieraf <elaman99619@gmail.com>
2025-12-25 16:32:09 +0300

    implement signup

---
+++ backend/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from user.models import CustomUser
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import SignupSerializer
 
 # Create your views here.
+class SignupView(CreateAPIView):
+    serializer_class = SignupSerializer

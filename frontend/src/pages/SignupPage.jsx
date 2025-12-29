4546213a8a6496843c6eec97151f15f6b0da61d2
mieraf <elaman99619@gmail.com>
2025-12-29 22:00:46 +0300

    fix bug

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import SignupForm from '../components/SignUpForm';
+import SignupForm from '../components/SignupForm';
 import './css/SignUpPage.css';
 
 function SignupPage() {
